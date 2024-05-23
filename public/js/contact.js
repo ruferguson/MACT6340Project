2e5ca47398b06fc6560cbd4d08c96942df20a472
ruferguson <ruf@smu.edu>
2024-05-23 05:50:06 -0700

    added form validity

--- public/js/contact.js
+++ public/js/contact.js
@@ -2,19 +2,29 @@
   "use strict";
 
   let form = document.querySelector('#contact-form')
-  
+
   document
     .querySelector("#contact-form-button")
-    .addEventListener('click', (event) => {
+    .addEventListener("click", (event) => {
       event.stopPropagation();
       event.preventDefault();
-      console.log("Contact submit button was clicked.");
-      let name = document.querySelector("#name").value;
-      let email = document.querySelector("#mail").value;
-      let message = document.querySelector("#msg").value;
-      console.log("Name: " + name);
-      console.log("Email: " + email);
-      console.log("Message: " + message);
+      let formValid = true;
+      if (!form.checkValidity()) {
+        formValid = false;
+      }
+      form.classList.add('was-validated');
+      if (formValid) {
+        sendTheEmail();
+      }
     });
+
+  function sendTheEmail() {
+    console.log("Contact submit button was clicked.");
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#mail").value;
+    let message = document.querySelector("#msg").value;
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Message: " + message);
+  }
 })();
-  
