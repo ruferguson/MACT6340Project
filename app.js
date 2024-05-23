3de65b88618d01aa6ab3d2a7ce4124ab8da620a3
ruferguson <ruf@smu.edu>
2024-05-23 06:40:35 -0700

    added utils and .env - which will not appear in github

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express";
-const app = express()
-const port = 3000
+import dotenv from "dotenv";
+dotenv.config();
 
+const app = express();
+const port = 3000;
+app.use(express.json());
 app.use(express.static("public"));
 
 // app.get('/', (req, res) => {
@@ -9,9 +12,10 @@ app.use(express.static("public"));
 // })
 
 app.post("/mail", (req, res) => {
-  console.log("mail button clicked")
+  console.log("mail button clicked");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(process.env.SENSITIVE_INFO);
+  console.log(`Example app listening on port ${port}`);
 })
