aece7889a669c62f2dbcb800aaa8033d5f0ce5a8
ruferguson <ruf@smu.edu>
2024-05-23 06:46:23 -0700

    updated app.js to support contact form email submissions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import * as utils from "./utils/utils";
 dotenv.config();
 
 const app = express();
@@ -11,8 +12,15 @@ app.use(express.static("public"));
 //   res.send('Hello World!')
 // })
 
-app.post("/mail", (req, res) => {
-  console.log("mail button clicked");
+app.post("/mail", async (req, res) => {
+  await utils
+    .sendMessage(req.body.sub, req.body.txt)
+    .then(() => {
+      res.send({result: "success"});
+    })
+    .catch(() => {
+      res.send({result: "failure"});
+    });
 });
 
 app.listen(port, () => {
