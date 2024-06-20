f68964fe2471a5ab38cfd9887c37554d593c1a6d
ruferguson <ruf@smu.edu>
2024-06-20 15:11:58 -0400

    added daisy filler information to the database and updated rest of code to use information from that

--- FergDev.session.sql
+++ FergDev.session.sql
@@ -1,1 +1,1 @@
-select * from projects;
+select * from filler;
--- app.js
+++ app.js
@@ -1,39 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
+import * as db from "./utils/database.js";
+
 dotenv.config();
-import * as db from "./utils/database.js"
-let data = ["Project 1", "Project 2", "Project 3"];
-let projects = [];
 
 const app = express();
 const port = 3000;
+
+let projects = [];
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
 app.get('/', async (req, res, next) => {
-  await db
-    .connect()
-    .then(async() => {
-      // query the database for project records
-      projects = await db.getAllProjects();
-      var randomNumber = Math.floor(Math.random() * projects.length);
-      res.render("index.ejs", {projectArray: data, featuredProj: projects[randomNumber]});
-    })
-    .catch(next);
+  try {
+    await db.connect();
+    projects = await db.getAllProjects();
+    if (projects.length > 0) {
+      const randomNumber = Math.floor(Math.random() * projects.length);
+      res.render("index.ejs", { projectArray: projects, featuredProj: projects[randomNumber] });
+    } else {
+      res.render("index.ejs", { projectArray: projects, featuredProj: null });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.get("/projects", (req, res) => {
-  res.render("projects.ejs", {projectArray: projects});
+  res.render("projects.ejs", { projectArray: projects });
 });
 
-app.get("/project/:id", (req, res) => {
-  let id = req.params.id;
-  if (id > data.length) {
-    throw new Error("No project found with that ID")
+app.get("/project/:id", (req, res, next) => {
+  const id = req.params.id;
+  if (id >= 0 && id < projects.length) {
+    res.render("project.ejs", { projectArray: projects, which: id });
+  } else {
+    const err = new Error("No project found with that ID");
+    next(err);
   }
-  res.render("project.ejs", {projectArray: data, which: id});
 });
 
 app.get("/contact", (req, res) => {
@@ -41,27 +48,29 @@ app.get("/contact", (req, res) => {
 });
 
 app.get('/index', (req, res) => {
-  var randomNumber = Math.floor(Math.random() * projects.length);
-  res.render("index.ejs", {projectArray: data, featuredProj: projects[randomNumber]});
+  if (projects.length > 0) {
+    const randomNumber = Math.floor(Math.random() * projects.length);
+    res.render("index.ejs", { projectArray: projects, featuredProj: projects[randomNumber] });
+  } else {
+    res.render("index.ejs", { projectArray: projects, featuredProj: null });
+  }
 });
 
 app.post("/mail", async (req, res) => {
-  await utils
-    .sendMessage(req.body.sub, req.body.txt)
-    .then(() => {
-      res.send({ result: "Your message was sent successfully!" });
-    })
-    .catch(() => {
-      res.send({ result: "Failure to send message." });
-    });
+  try {
+    await utils.sendMessage(req.body.sub, req.body.txt);
+    res.send({ result: "Your message was sent successfully!" });
+  } catch (err) {
+    console.error(err);
+    res.send({ result: "Failure to send message." });
+  }
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
+  console.error(err);
   res.render("error.ejs");
 });
 
 app.listen(port, () => {
-  console.log(process.env.SENSITIVE_INFO);
   console.log(`Example app listening on port ${port}`);
 });
